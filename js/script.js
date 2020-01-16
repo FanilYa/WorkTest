3cb062a83d5e8f3ee8b637389770c356f98b0ac4
FanilKit <fanilkit7567@gmail.com>
2020-01-16 22:09:44 +0500

    a few changes

--- js/script.js
+++ js/script.js
@@ -1,12 +1,26 @@
 $(document).ready(function() {
     let bar = $('.bars'),
-        modalWindow = $('.main_menu_header');
+        modalWindow = $('.main_menu_header'),
+        modalBtn = $('.show-modal'),
+        showModal = $('.modal_form');
 
     bar.click(function() {
         modalWindow.removeClass('hide');
+        $("html,body").css("overflow","hidden");
     });
 
     modalWindow.click(function () {
         modalWindow.addClass('hide');
+        $("html,body").css("overflow","");
+    });
+
+    modalBtn.click(function() {
+        $("html,body").css("overflow","hidden");
+        showModal.removeClass('hide');
+    });
+
+    showModal.click(function() {
+        showModal.addClass('hide');
+        $("html,body").css("overflow","");
     });
 });
